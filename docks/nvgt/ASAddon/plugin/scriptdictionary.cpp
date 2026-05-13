6677b8ad6cbbe62d9b0ac82d62e3e8a5b5d6f59e
Igor Alves <igoralvespalmeirag4@gmail.com>
2026-05-13 12:02:27 -0300

    muita alteração feita,

--- docks/nvgt/ASAddon/plugin/scriptdictionary.cpp
+++
@@ -1,3 +0,0 @@
-#define NVGT_PLUGIN_INCLUDE
-#include "../../src/nvgt_plugin.h"
-#include "../src/scriptdictionary.cpp"
--- docks/nvgt/dep/InputBox.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <windows.h>
-#include <string>
-
-std::wstring InputBox(const std::wstring& szCaption, const std::wstring& szPrompt, const std::wstring& szDefaultText = L"", HWND hWnd = NULL);
-BOOL InfoBox(const std::wstring& szCaption, const std::wstring& szPrompt, const std::wstring& szText, HWND hWnd=NULL);
